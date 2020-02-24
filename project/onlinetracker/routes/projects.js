9434db553b1347ed76b93f82a036dadf4d7b8f0c
Chia-Ying Ho <42313352+iosum@users.noreply.github.com>
2020-02-23 20:33:13 -0500

    create

--- project/onlinetracker/routes/projects.js
+++ project/onlinetracker/routes/projects.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const Project = require('../models/project');
 
 router.get('/', (req, res, next) => {
-    // use the Project model & mongoose to select all the projects from MongoDB
+    // use the Project model & mongoose to select(read) all the projects from MongoDB
     Project.find((err, projects) => {
       if(err) {
           console.log(err);
@@ -18,6 +18,31 @@ router.get('/', (req, res, next) => {
           });
       }
     });
+});
+
+router.get('/add',(req, res, next) => {
+    // load the add view
+    res.render('projects/add')
+});
+
+router.post('/add',(req, res, next) => {
+   // create a new document in the projects collection using the project model
+    Project.create({
+        // get the data from the form
+        name: req.body.projectName,
+        description: req.body.projectDescription
+    }, (err, newProject)=> {
+        // if we get an error while storing the data
+        if(err) {
+            // log the error to the console
+            console.log(err);
+        }
+        // otherwise, the data is stored successfully and we can redirect to the main page
+        else {
+            // load the updated foods index
+            res.redirect('/projects');
+        }
+    })
 });
 
 // make the controller public
